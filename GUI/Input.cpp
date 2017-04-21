f31d2e6ada728965937ac726505ce1f2eebfe1e0
Mohamed Taha <Mohamed Taha>
2017-04-21 22:11:14 +0200

    First commit

---
+++ GUI/Input.cpp
@@ -0,0 +1,206 @@
+#include "Input.h"
+#include "Output.h"
+
+
+Input::Input(window* pW) 
+{
+	pWind = pW; //point to the passed window
+}
+
+void Input::GetPointClicked(int &x, int &y) const
+{
+	pWind->WaitMouseClick(x, y);	//Wait for mouse click
+}
+
+string Input::GetSrting(Output *pO) const 
+{
+	string Label;
+	char Key;
+	while(1)
+	{
+		pWind->WaitKeyPress(Key);
+		if(Key == 27 )	//ESCAPE key is pressed
+			return "";	//returns nothing as user has cancelled label
+		if(Key == 13 )	//ENTER key is pressed
+			return Label;
+		if(Key == 8 )	//BackSpace is pressed
+			Label.resize(Label.size() -1 );			
+		else
+			Label+= Key;
+		pO->PrintMessage(Label);
+	}
+}
+
+//This function reads the position where the user clicks to determine the desired action
+ActionType Input::GetUserAction() const
+{
+	int x, y;
+	pWind->WaitMouseClick(x, y);	//Get the coordinates of the user click
+
+	if (UI.InterfaceMode == MODE_DRAW)	//GUI in the DRAW mode
+	{
+		//[1] If user clicks on the Toolbar
+		if (y >= 0 && y < UI.ToolBarHeight)
+		{
+			//Check whick Menu item was clicked
+			//==> This assumes that menu items are lined up horizontally <==
+			int ClickedItemOrder = (x / UI.MenuItemWidth);
+			//Divide x coord of the point clicked by the menu item width (int division)
+			//if division result is 0 ==> first item is clicked, if 1 ==> 2nd item and so on
+
+			switch (ClickedItemOrder)
+			{
+			case ITM_RECT:						{ return DRAW_RECT;} // look here ya taha ya 312.
+			case ITM_CIRC:						{ return DRAW_CIRC;}
+			case ITM_LINE:						{ return DRAW_LINE;}
+			case ITM_TRI:						{ return DRAW_TRI;}
+			case ITM_CHNG_DRAW_CLR:				{ return	CHNG_DRAW_CLR;}
+			case ITM_CHNG_FILL_CLR:				{ return	CHNG_FILL_CLR;}	//Change the filling color
+			case ITM_CHNG_BK_CLR:				{ return	CHNG_BK_CLR;} //Change background color
+			case ITM_CHNG_BRDR_WDTH:			{ return CHNG_BRDR_WDTH; }
+			case ITM_DEL:						{ return	DEL;	}	//Delete a figure(s)
+			case ITM_SAVE:						{ return	SAVE;	}	//Save the whole graph to a file
+			case ITM_LOAD:						{ return	LOAD;	}	//Load a graph from a file
+			case ITM_SWITCH_PLAY:				{ return TO_PLAY; } // SWITCHES FROM DRAW MODE TO PLAY MODE YA RIAD YA 312
+			case ITM_SELECT:					{ return TO_SELECT;}
+			case ITM_CUT:						{ return CUT; }
+			case ITM_COPY:						{ return COPY; }
+			case ITM_RESIZE:					{ return RESIZE; }
+			case ITM_MOVE:						{ return MOVE; }
+			case ITM_ZOOMIN:					{ return ZOOMIN; }
+			case ITM_ZOOMOUT:					{ return ZOOMOUT; }
+			case ITM_PASTE:						{ return PASTE; }
+			case ITM_EXIT:						{ return EXIT;}
+
+
+				/*	//	case ITM_MOVE:  return	MOVE,			//Move a figure(s)
+						//   case ITM_RESIZE:  return	RESIZE,			//Resize a figure(s)
+			//    case ITM_ROTATE:  return	ROTATE,			//Rotate a figure(s)
+					//	case ITM_SEND_BACK:  return	SEND_BACK,		//Send a figure to the back of all figures
+					//	case ITM_BRNG_FRNT:  return	BRNG_FRNT,		//Bring a figure to the front of all figures*/
+			default: return EMPTY;	//A click on empty place in desgin toolbar
+			}
+		}
+
+		//[2] User clicks on the drawing area
+		if (y >= UI.ToolBarHeight && y < UI.height - UI.StatusBarHeight)
+		{
+			return DRAWING_AREA;
+		}
+
+		//[3] User clicks on the status bar
+		return STATUS;
+	}
+	else if (UI.InterfaceMode == MODE_PLAY)	//GUI is in PLAY mode
+	{
+		//[1] If user clicks on the Toolbar
+		if (y >= 0 && y < UI.ToolBarHeight)
+		{
+			//Check whick Menu item was clicked
+			//==> This assumes that menu items are lined up horizontally <==
+			int ClickedItemOrder = (x / UI.MenuItemWidth);
+			//Divide x coord of the point clicked by the menu item width (int division)
+			//if division result is 0 ==> first item is clicked, if 1 ==> 2nd item and so on
+
+			switch (ClickedItemOrder)
+			{
+			case ITM_SCRAMBLE_FIND: return TO_SCRAMBLE_FIND;
+			case ITM_PICK_HIDE: return TO_PICK_HIDE;
+			case ITM_EXIT_PLAY: return EXIT;
+			case ITM_SWITCH_DRAW: return TO_DRAW;
+			default: return EMPTY;	//A click on empty place in desgin toolbar
+			}
+			//[2] User clicks on the drawing area
+			if (y >= UI.ToolBarHeight && y < UI.height - UI.StatusBarHeight)
+			{
+				return DRAWING_AREA;
+			}
+
+			//[3] User clicks on the status bar
+			return STATUS;
+			///TODO:
+			//perform checks similar to Draw mode checks above
+			//and return the correspoding action
+
+		}
+
+	}
+
+}
+/////////////////////////////////
+color Input::ChangeColor() 
+{
+	f:
+	int x, y;
+	color c = SNOW;
+	pWind->WaitMouseClick(x, y);	//Get the coordinates of the user click
+	if (y >= 0 && y < UI.ToolBarHeight)
+	{
+		//Check whick Menu item was clicked
+		//==> This assumes that menu items are lined up horizontally <==
+		int ClickedItemOrder = (x / UI.MenuItemWidth);
+		//Divide x coord of the point clicked by the menu item width (int division)
+		//if division result is 0 ==> first item is clicked, if 1 ==> 2nd item and so on
+		switch (ClickedItemOrder)
+		{
+		case ITM_BLACK: { c = BLACK;   }
+						break;
+		case ITM_BLUE: { c = BLUE; }
+					   break;
+		case ITM_RED: {  c = RED; }
+					  break;
+		case ITM_GREEN: { c = GREEN; }
+						break;
+		case ITM_BROWN: { c = BROWN; }
+					   break;
+		case ITM_BLUEVIOLET: {  c = BLUEVIOLET; }
+					  break;
+		case ITM_CYAN: { c = CYAN; }
+						break;
+		case ITM_DARKGREEN: { c = DARKGREEN; }
+					   break;
+		case ITM_VIOLET: {  c = VIOLET; }
+					  break;
+		case ITM_GRAY: { c = GRAY; }
+						break;
+		case ITM_ORANGE: { c = ORANGE; }
+					   break;
+		case ITM_PINK: {  c = PINK; }
+					  break;
+		case ITM_SEAGREEN: { c = SEAGREEN; }
+						break;
+		case ITM_YELLOW: { c = YELLOW; }
+						break;
+		default:goto f;
+		}
+		
+	}
+	else {
+		goto f;
+	}
+	return c;
+}
+
+int Input::IsFilled() const
+{
+	f:
+	int x, y;
+	pWind->WaitMouseClick(x, y);	//Get the coordinates of the user click
+	if (y >= 0 && y < UI.ToolBarHeight)
+	{
+		//Check whick Menu item was clicked
+		//==> This assumes that menu items are lined up horizontally <==
+		int ClickedItemOrder = (x / UI.MenuItemWidth);
+		if (ClickedItemOrder<3)
+		{
+			return ClickedItemOrder;
+		}
+			goto f;
+			
+	}
+	goto f;
+}
+
+Input::~Input()
+{
+}
---
+++ GUI/Output.cpp
@@ -0,0 +1,345 @@
+#include "Output.h"
+
+
+Output::Output()
+{
+	//Initialize user interface parameters
+	UI.InterfaceMode = MODE_DRAW;
+	
+	UI.width = 1300;
+	UI.height = 700;
+	UI.wx = 5;
+	UI.wy =5;
+
+	
+	UI.StatusBarHeight = 50;
+	UI.ToolBarHeight = 50;
+	UI.MenuItemWidth = 61;
+	
+	UI.DrawColor = BLUE;	//Drawing color
+	UI.FillColor = GREEN;	//Filling color
+	UI.MsgColor = RED;		//Messages color
+	UI.BkGrndColor = LIGHTGOLDENRODYELLOW;	//Background color
+	UI.HighlightColor = MAGENTA;	//This color should NOT be used to draw figures. use if for highlight only
+	UI.StatusBarColor = TURQUOISE;
+	UI.PenWidth = 3;	//width of the figures frames
+
+	
+	//Create the output window
+	pWind = CreateWind(UI.width, UI.height, UI.wx, UI.wy);
+	//Change the title
+	pWind->ChangeTitle("Paint for Kids - Programming Techniques Project");
+	
+	CreateDrawToolBar();
+	CreateStatusBar();
+}
+
+
+Input* Output::CreateInput() const
+{
+	Input* pIn = new Input(pWind);
+	return pIn;
+}
+
+//======================================================================================//
+//								Interface Functions										//
+//======================================================================================//
+
+window* Output::CreateWind(int w, int h, int x, int y) const
+{ 
+	window* pW = new window(w, h, x, y);
+	pW->SetBrush(UI.BkGrndColor);
+	pW->SetPen(UI.BkGrndColor, 1);
+	pW->DrawRectangle(0, UI.ToolBarHeight, w, h);	
+	return pW;
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::
+CreateStatusBar() const
+{
+	pWind->SetPen(UI.StatusBarColor, 1);
+	pWind->SetBrush(UI.StatusBarColor);
+	pWind->DrawRectangle(0, UI.height - UI.StatusBarHeight, UI.width, UI.height);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::ClearStatusBar() const
+{
+	//Clear Status bar by drawing a filled white rectangle
+	pWind->SetPen(UI.StatusBarColor, 1);
+	pWind->SetBrush(UI.StatusBarColor);
+	pWind->DrawRectangle(0, UI.height - UI.StatusBarHeight, UI.width, UI.height);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::ClearToolBar() const
+{
+	//Clear Status bar by drawing a filled white rectangle
+	pWind->SetPen(UI.BkGrndColor, 1);
+	pWind->SetBrush(UI.BkGrndColor);
+	pWind->DrawRectangle(0, 0, 1300, 50);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::CreateDrawToolBar() const
+{
+	ClearToolBar();
+	UI.InterfaceMode = MODE_DRAW;
+
+	//You can draw the tool bar icons in any way you want.
+	//Below is one possible way
+	
+	//First prepare List of images for each menu item
+	//To control the order of these images in the menu, 
+	//reoder them in UI_Info.h ==> enum DrawMenuItem
+	string MenuItemImages[DRAW_ITM_COUNT];
+	MenuItemImages[ITM_LINE]		   = "images\\MenuItems\\Line.jpg";
+	MenuItemImages[ITM_RECT]           = "images\\MenuItems\\Rectangle.jpg";
+	MenuItemImages[ITM_CIRC]		   = "images\\MenuItems\\Circle.jpg";
+	MenuItemImages[ITM_TRI]            = "images\\MenuItems\\Triangle.jpg";
+	MenuItemImages[ITM_CHNG_DRAW_CLR]  = "images\\MenuItems\\colors.jpg";
+	MenuItemImages[ITM_CHNG_FILL_CLR]  = "images\\MenuItems\\Fill.jpg";
+	MenuItemImages[ITM_CHNG_BK_CLR]    = "images\\MenuItems\\Background.jpg";
+	MenuItemImages[ITM_DEL]            = "images\\MenuItems\\Delete.jpg";
+	MenuItemImages[ITM_SAVE]           = "images\\MenuItems\\save.jpg";
+	MenuItemImages[ITM_LOAD]           = "images\\MenuItems\\Load.jpg";
+	MenuItemImages[ITM_EXIT]           = "images\\MenuItems\\exit.jpg";
+	MenuItemImages[ITM_SWITCH_PLAY]    = "images\\MenuItems\\to play.jpg";
+	MenuItemImages[ITM_SELECT]		   = "images\\MenuItems\\Select.jpg";
+	MenuItemImages[ITM_COPY]		   = "images\\MenuItems\\Copy.jpg";
+	MenuItemImages[ITM_CUT]			   = "images\\MenuItems\\Cut.jpg";
+	MenuItemImages[ITM_RESIZE]		   = "images\\MenuItems\\Resize.jpg";
+	MenuItemImages[ITM_MOVE]		   = "images\\MenuItems\\Move.jpg";
+	MenuItemImages[ITM_PASTE]		   = "images\\MenuItems\\Paste.jpg";
+	MenuItemImages[ITM_ZOOMOUT]		   = "images\\MenuItems\\Zoom Out.jpg";
+	MenuItemImages[ITM_ZOOMIN]		   = "images\\MenuItems\\Zoom In.jpg";
+	MenuItemImages[ITM_CHNG_BRDR_WDTH] = "images\\MenuItems\\Line Width.jpg";
+	//TODO: Prepare images for each menu item and add it to the list
+
+	//Draw menu item one image at a time
+	for(int i=0; i<DRAW_ITM_COUNT; i++)
+		pWind->DrawImage(MenuItemImages[i], i*UI.MenuItemWidth,0,UI.MenuItemWidth, UI.ToolBarHeight);
+
+
+
+	//Draw a line under the toolbar
+	pWind->SetPen(RED, 3);
+	pWind->DrawLine(0, UI.ToolBarHeight, UI.width, UI.ToolBarHeight);	
+
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::CreateColorToolBar() const
+{
+	ClearToolBar();
+
+	//You can draw the tool bar icons in any way you want.
+	//Below is one possible way
+
+	//First prepare List of images for each menu item
+	//To control the order of these images in the menu, 
+	//reoder them in UI_Info.h ==> enum DrawMenuItem
+	string MenuItemImages[ITM_COLOR_COUNT];
+	MenuItemImages[0] = "images\\MenuItems\\Black.jpg";
+	MenuItemImages[1] = "images\\MenuItems\\Blue.jpg";
+	MenuItemImages[2] = "images\\MenuItems\\Red.jpg";
+	MenuItemImages[3] = "images\\MenuItems\\Green.jpg";
+	MenuItemImages[4] = "images\\MenuItems\\Brown.jpg";
+	MenuItemImages[5] = "images\\MenuItems\\BlueViolet.jpg";
+	MenuItemImages[6] = "images\\MenuItems\\Cyan.jpg";
+	MenuItemImages[7] = "images\\MenuItems\\DarkGreen.jpg";
+	MenuItemImages[8] = "images\\MenuItems\\Violet.jpg";
+	MenuItemImages[9] = "images\\MenuItems\\Gray.jpg";
+	MenuItemImages[10] = "images\\MenuItems\\Orange.jpg";
+	MenuItemImages[11] = "images\\MenuItems\\Pink.jpg";
+	MenuItemImages[12] = "images\\MenuItems\\SeaGreen.jpg";
+	MenuItemImages[13] = "images\\MenuItems\\Yellow.jpg";
+
+	//TODO: Prepare images for each menu item and add it to the list
+
+	//Draw menu item one image at a time
+	for (int i = 0; i<ITM_COLOR_COUNT; i++)
+		pWind->DrawImage(MenuItemImages[i], i*UI.MenuItemWidth, 0, UI.MenuItemWidth, UI.ToolBarHeight);
+
+
+
+	//Draw a line under the toolbar
+	pWind->SetPen(RED, 3);
+	pWind->DrawLine(0, UI.ToolBarHeight, UI.width, UI.ToolBarHeight);
+
+}
+void Output::CreatePlayToolBar() const
+{
+	ClearToolBar();
+	UI.InterfaceMode = MODE_PLAY;
+
+	//You can draw the tool bar icons in any way you want.
+	//Below is one possible way
+
+	//First prepare List of images for each menu item
+	//To control the order of these images in the menu, 
+	//reoder them in UI_Info.h ==> enum DrawMenuItem
+	string MenuItemImages[PLAY_ITM_COUNT];
+	MenuItemImages[ITM_PICK_HIDE] = "images\\MenuItems\\pick.jpg";
+	MenuItemImages[ITM_SCRAMBLE_FIND] = "images\\MenuItems\\scramble.jpg";
+	MenuItemImages[ITM_EXIT_PLAY] = "images\\MenuItems\\exit.jpg";
+	MenuItemImages[ITM_SWITCH_DRAW] = "images\\MenuItems\\to draw.jpg";
+
+
+	//TODO: Prepare images for each menu item and add it to the list
+
+	//Draw menu item one image at a time
+	for (int i = 0; i<PLAY_ITM_COUNT; i++)
+		pWind->DrawImage(MenuItemImages[i], i*UI.MenuItemWidth, 0, UI.MenuItemWidth, UI.ToolBarHeight);
+
+
+
+	//Draw a line under the toolbar
+	pWind->SetPen(RED, 3);
+	pWind->DrawLine(0, UI.ToolBarHeight, UI.width, UI.ToolBarHeight);
+
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::CreateFigMenu() const
+{
+	ClearToolBar();
+
+	string MenuItemImages[2];
+	MenuItemImages[0] = "images\\MenuItems\\Black.jpg";
+	MenuItemImages[1] = "images\\MenuItems\\Blue.jpg";
+	for (int i = 0; i<2; i++)
+		pWind->DrawImage(MenuItemImages[i], i*UI.MenuItemWidth, 0, UI.MenuItemWidth, UI.ToolBarHeight);
+
+	//Draw a line under the toolbar
+	pWind->SetPen(RED, 3);
+	pWind->DrawLine(0, UI.ToolBarHeight, UI.width, UI.ToolBarHeight);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+void Output::ClearDrawArea() const
+{
+	pWind->SetPen(UI.BkGrndColor, 1);
+	pWind->SetBrush(UI.BkGrndColor);
+	pWind->DrawRectangle(0, UI.ToolBarHeight, UI.width, UI.height - UI.StatusBarHeight);
+	
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+
+void Output::PrintMessage(string msg) const	//Prints a message on status bar
+{
+	ClearStatusBar();	//First clear the status bar
+	
+	pWind->SetPen(UI.MsgColor, 50);
+	pWind->SetFont(20, BOLD , BY_NAME, "Arial");   
+	pWind->DrawString(10, UI.height - (int)(UI.StatusBarHeight/1.5), msg);
+}
+//////////////////////////////////////////////////////////////////////////////////////////
+
+color Output::getCrntDrawColor() const	//get current drwawing color
+{	return UI.DrawColor;	}
+//////////////////////////////////////////////////////////////////////////////////////////
+
+color Output::getCrntFillColor() const	//get current filling color
+{	return UI.FillColor;	}
+//////////////////////////////////////////////////////////////////////////////////////////
+	
+int Output::getCrntPenWidth() const		//get current pen width
+{	return UI.PenWidth;	}
+
+//======================================================================================//
+//								Figures Drawing Functions								//
+//======================================================================================//
+
+void Output::DrawRect(Point P1, Point P2, GfxInfo RectGfxInfo, bool selected) const
+{
+	color DrawingClr;
+	if(selected)	
+		DrawingClr = UI.HighlightColor; //Figure should be drawn highlighted
+	else			
+		DrawingClr = RectGfxInfo.DrawClr;
+	
+	pWind->SetPen(DrawingClr, RectGfxInfo.BorderWdth);	//Set Drawing color & width
+
+	drawstyle style;
+	if (RectGfxInfo.isFilled)	
+	{
+		style = FILLED;		
+		pWind->SetBrush(RectGfxInfo.FillClr);
+	}
+	else	
+		style = FRAME;
+
+	
+	pWind->DrawRectangle(P1.x, P1.y, P2.x, P2.y, style);
+	
+}
+void Output::DrawTRI(Point P1, Point P2,Point P3, GfxInfo RectGfxInfo, bool selected) const
+{
+	color DrawingClr;
+	if (selected)
+		DrawingClr = UI.HighlightColor; //Figure should be drawn highlighted
+	else
+		DrawingClr = RectGfxInfo.DrawClr;
+
+	pWind->SetPen(DrawingClr, RectGfxInfo.BorderWdth);	//Set Drawing color & width
+
+	drawstyle style;
+	if (RectGfxInfo.isFilled)
+	{
+		style = FILLED;
+		pWind->SetBrush(RectGfxInfo.FillClr);
+	}
+	else
+		style = FRAME;
+
+
+	pWind->DrawTriangle(P1.x, P1.y, P2.x, P2.y,P3.x,P3.y, style);
+}
+void Output::DrawLINE(Point P1, Point P2, GfxInfo RectGfxInfo, bool selected) const
+{
+	color DrawingClr;
+	if (selected)
+		DrawingClr = UI.HighlightColor; //Figure should be drawn highlighted
+	else
+		DrawingClr = RectGfxInfo.DrawClr;
+
+	pWind->SetPen(DrawingClr, RectGfxInfo.BorderWdth);	//Set Drawing color & width
+
+	drawstyle style;
+	if (RectGfxInfo.isFilled)
+	{
+		style = FILLED;
+		pWind->SetBrush(RectGfxInfo.FillClr);
+	}
+	else
+		style = FRAME;
+
+
+	pWind->DrawLine(P1.x, P1.y, P2.x, P2.y);
+}
+void Output::DrawCIRC(Point P1, Point P2, GfxInfo RectGfxInfo, bool selected) const
+{
+	double r = sqrt( ((P2.y - P1.y) *(P2.y - P1.y)) + ((P2.x - P1.x)*(P2.x - P1.x)));
+	color DrawingClr;
+	if (selected)
+		DrawingClr = UI.HighlightColor; //Figure should be drawn highlighted
+	else
+		DrawingClr = RectGfxInfo.DrawClr;
+
+	pWind->SetPen(DrawingClr, RectGfxInfo.BorderWdth);	//Set Drawing color & width
+
+	drawstyle style;
+	if (RectGfxInfo.isFilled)
+	{
+		style = FILLED;
+		pWind->SetBrush(RectGfxInfo.FillClr);
+	}
+	else
+		style = FRAME;
+
+
+	pWind->DrawCircle(P1.x, P1.y, r, style);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+Output::~Output()
+{
+	delete pWind;
+}
+
