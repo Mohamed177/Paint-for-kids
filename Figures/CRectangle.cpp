9ee6581052059a7423756364089fb1659a525234
Mohamed Taha <Mohamed Taha>
2017-04-25 14:11:40 +0200

    Rec Save Func

--- Figures/CRectangle.cpp
+++ Figures/CRectangle.cpp
@@ -40,3 +40,71 @@ bool CRectangle::Is_Selected(Point P) const
 		return true;
 	return false;
 }
+
+void CRectangle::Save(ofstream &OutFile)
+{
+	OutFile << "Rectangle " << ID << ' ' << Corner1.x << ' ' << Corner1.y << ' ' << Corner2.x << ' ' << Corner2.y << ' ';
+	if (FigGfxInfo.DrawClr == BLACK)
+		OutFile << "BLACK ";
+	else if (FigGfxInfo.DrawClr == BLUE)
+		OutFile << "BLUE ";
+	else if (FigGfxInfo.DrawClr == RED)
+		OutFile << "RED ";
+	else if (FigGfxInfo.DrawClr == DARKGREEN)
+		OutFile << "DARKGREEN ";
+	else if (FigGfxInfo.DrawClr == GREEN)
+		OutFile << "GREEN ";
+	else if (FigGfxInfo.DrawClr == BROWN)
+		OutFile << "BROWN ";
+	else if (FigGfxInfo.DrawClr == BLUEVIOLET)
+		OutFile << "BLUEVIOLET ";
+	else if (FigGfxInfo.DrawClr == CYAN)
+		OutFile << "CYAN ";
+	else if (FigGfxInfo.DrawClr == VIOLET)
+		OutFile << "VIOLET ";
+	else if (FigGfxInfo.DrawClr == GRAY)
+		OutFile << "GRAY ";
+	else if (FigGfxInfo.DrawClr == ORANGE)
+		OutFile << "ORANGE ";
+	else if (FigGfxInfo.DrawClr == PINK)
+		OutFile << "PINK ";
+	else if (FigGfxInfo.DrawClr == SEAGREEN)
+		OutFile << "SEAGREEN ";
+	else if (FigGfxInfo.DrawClr == YELLOW)
+		OutFile << "YELLOW ";
+	if (FigGfxInfo.isFilled)
+		OutFile << "NO_FILL\n";
+	else
+	{
+		if (FigGfxInfo.FillClr == BLACK)
+			OutFile << "BLACK\n";
+		else if (FigGfxInfo.FillClr == BLUE)
+			OutFile << "BLUE\n";
+		else if (FigGfxInfo.FillClr == RED)
+			OutFile << "RED\n";
+		else if (FigGfxInfo.FillClr == DARKGREEN)
+			OutFile << "DARKGREEN\n";
+		else if (FigGfxInfo.FillClr == GREEN)
+			OutFile << "GREEN\n";
+		else if (FigGfxInfo.FillClr == BROWN)
+			OutFile << "BROWN\n";
+		else if (FigGfxInfo.FillClr == BLUEVIOLET)
+			OutFile << "BLUEVIOLET\n";
+		else if (FigGfxInfo.FillClr == CYAN)
+			OutFile << "CYAN\n";
+		else if (FigGfxInfo.FillClr == VIOLET)
+			OutFile << "VIOLET\n";
+		else if (FigGfxInfo.FillClr == GRAY)
+			OutFile << "GRAY\n";
+		else if (FigGfxInfo.FillClr == ORANGE)
+			OutFile << "ORANGE\n";
+		else if (FigGfxInfo.FillClr == PINK)
+			OutFile << "PINK\n";
+		else if (FigGfxInfo.FillClr == SEAGREEN)
+			OutFile << "SEAGREEN\n";
+		else if (FigGfxInfo.FillClr == YELLOW)
+			OutFile << "YELLOW\n";
+		if (FigGfxInfo.isFilled)
+			OutFile << "NO_FILL\n";
+	}
+}
--- Figures/CRectangle.h
+++ Figures/CRectangle.h
@@ -2,6 +2,7 @@
 #define CRECT_H
 
 #include "CFigure.h"
+#include <fstream>
 
 class CRectangle : public CFigure
 {
@@ -12,6 +13,7 @@ public:
 	CRectangle(Point , Point, GfxInfo FigureGfxInfo );
 	virtual void Draw(Output* pOut) const;
 	bool Is_Selected(Point P) const;
+	void Save(ofstream &OutFile);
 };
 
 #endif
